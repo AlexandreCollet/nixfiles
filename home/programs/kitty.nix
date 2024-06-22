8a926f97d97e35a21c349e2eee15ce658a00a6a9
Alexandre Collet <3273308+AlexandreCollet@users.noreply.github.com>
2024-06-22 08:57:46 +0200

    update kitty settings colors

--- home/programs/kitty.nix
+++ home/programs/kitty.nix
@@ -6,5 +6,10 @@
         confirm_os_window_close = 0;
         window_padding_width = 5;
         shell = "zsh";
+        background            = "#000000";
+        foreground            = "#d6dae4";
+        cursor                = "#b9b9b9";
+        selection_background  = "#d6dae4";
+        selection_foreground  = "#000000";
     };
 }
